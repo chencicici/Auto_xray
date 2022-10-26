740c8768d5e5a57d2c3d4dfdf7b324c51407ecba
chencicici <1343369483@qq.com>
2022-10-26 23:24:23 +0800

    test

--- my_email.py
+++ my_email.py
@@ -18,9 +18,7 @@ title = '叮~ 漏洞扫描推送'  # 邮件主题
 # 发送邮件
 def send_mail(run_time):
     try:
-        yagmail.SMTP(user=sender_email, password=sender_pass, host='smtp.qq.com', port=465).send(to=receiver_mail,
-                                                                                                 subject=title,
-                                                                                                 contents=f'漏洞扫描已完成!\n耗时:{run_time}')
+        yagmail.SMTP(user=sender_email, password=sender_pass, host='smtp.qq.com', port=465).send(to=receiver_mail,subject=title,contents=f'漏洞扫描已完成!\n耗时:{run_time}')
         print('邮件已发送!')
     except Exception as e:
         print(e)
